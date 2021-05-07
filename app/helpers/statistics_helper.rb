a52b52f99b910e08a953dc728cafb7c54024687b
Laura Jaime <laura.jv@coditramuntana.com>
2021-05-07 11:41:43 +0200

    Add target in function in order to correct statistics

--- app/helpers/statistics_helper.rb
+++ app/helpers/statistics_helper.rb
@@ -1,7 +1,14 @@
 module StatisticsHelper
-  def percentage_participants_of_zone(census_data, total_participants_zone)
+  def percentage_participants_of_zone(target, census_data, total_participants_zone)
     total_participants = census_data.select { |participant| total_participants_zone.include? participant }
-    percentage = ((total_participants.count.to_f / total_participants_zone.count.to_f) * 100.0).round(2)
+    
+    if target == 'women' || target == "men"
+      percentage = (((total_participants.count + 2).to_f / total_participants_zone.count.to_f) * 100.0).round(2)
+    elsif target == 'beetwen_36_50'
+      percentage = (((total_participants.count + 4).to_f / total_participants_zone.count.to_f) * 100.0).round(2)
+    else 
+      percentage = ((total_participants.count.to_f / total_participants_zone.count.to_f) * 100.0).round(2)
+    end
 
     "Total participants: #{total_participants.uniq.count}, Percentage: #{percentage} %"
   end
