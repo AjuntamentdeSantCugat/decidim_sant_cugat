2568d0e04f3b2625f3a774121a06849fbf15374b
Laura Jaime <laura.jv@coditramuntana.com>
2021-05-07 13:26:53 +0200

    Remove target

--- lib/statistics.rb
+++ lib/statistics.rb
@@ -1,14 +1,7 @@
 module Statistics
-  def percentage_participants_of_zone(target, census_data, total_participants_zone)
+  def percentage_participants_of_zone(census_data, total_participants_zone)
     total_participants = census_data.select { |participant| total_participants_zone.include? participant }
-    
-    if target == 'women' || target == "men"
-      percentage = (((total_participants.count + 2).to_f / total_participants_zone.count.to_f) * 100.0).round(2)
-    elsif target == 'beetwen_36_50'
-      percentage = (((total_participants.count + 4).to_f / total_participants_zone.count.to_f) * 100.0).round(2)
-    else 
-      percentage = ((total_participants.count.to_f / total_participants_zone.count.to_f) * 100.0).round(2)
-    end
+    percentage = ((total_participants.count.to_f / total_participants_zone.count.to_f) * 100.0).round(2)
 
     "Total participants: #{total_participants.uniq.count}, Percentage: #{percentage} %"
   end
